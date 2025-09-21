097ffea281f0cc29da958f28010badafccac2ac3
ricardosantosfc <58186574+ricardosantosfc@users.noreply.github.com>
2025-09-21 17:41:32 +0100

    who : logic done. needs refactoring, mobile responsiveness for choice buttons

--- homepage/src/app/who/who.ts
+++ homepage/src/app/who/who.ts
@@ -1,5 +1,12 @@
-import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { Component, ElementRef, HostListener, signal, ViewChild } from '@angular/core';
 
+interface Dialogs {
+  text: string;
+  spriteAlternations: number; //times to alternate aimating between the sprites before end sprite
+  sprites: number[]; //spirtes?: 
+  endSprite: number; //the sprite on which to end the animation
+  nextDialogIndex: number; //index of the nex t dialog
+}
 @Component({
   selector: 'app-who',
   imports: [],
@@ -14,19 +21,194 @@ export class Who {
   private width = 0;
   private height = 0;
 
+  //closed mouth = uneven sptrites, excpet for 33,34
+  private dialogs: Dialogs[] = [
+    {
+      text: "...",
+      spriteAlternations: 0,
+      sprites: [0],
+      endSprite: 0,
+      nextDialogIndex: 1
+    },
+    {
+      text: "Hi, I'm Ricardo Santos!",
+      spriteAlternations: 2,
+      sprites: [1, 2, 3, 4],
+      endSprite: 0,
+      nextDialogIndex: 2
+    },
+    {
+      text: "I'm a software engineer from Lisbon, Portugal.",
+      spriteAlternations: 4,
+      sprites: [5, 6],
+      endSprite: 6,
+      nextDialogIndex: 3
+    },
+    {
+      text: "I enjoy creating full-stack applications with rich, highly interactive frontends...", //apps interesctions diff
+      spriteAlternations: 6,
+      sprites: [7, 8],
+      endSprite: 8,
+      nextDialogIndex: 4
+    },
+    {
+      text: "... and I'm also very interested in game development.",
+      spriteAlternations: 5,
+      sprites: [9, 10],
+      endSprite: 10,
+      nextDialogIndex: 5
+    },
+    {
+      text: "That being said, I'm always up to learn new things, especially if they're out of my comfort zone!",
+      spriteAlternations: 9,
+      sprites: [11, 12],
+      endSprite: 12,
+      nextDialogIndex: 6
+    },
+    {
+      text: "When I'm not working on software, I'm likely making music, 3D modeling, watercolor painting, or sketching.",
+      spriteAlternations: 10,
+      sprites: [33, 34],
+      endSprite: 33,
+      nextDialogIndex: 7
+    },
+    {
+      text: "Just give me a second please...",
+      spriteAlternations: 3,
+      sprites: [13, 14],
+      endSprite: 14,
+      nextDialogIndex: 8
+    },
+    {
+      text: "I have some copies of my resume here. ",
+      spriteAlternations: 5,
+      sprites: [15, 16],
+      endSprite: 16,
+      nextDialogIndex: 9
+    },
+    {
+      text: "Would you like to have one? ",
+      spriteAlternations: 5,
+      sprites: [17, 18],
+      endSprite: 18,
+      nextDialogIndex: 10
+    },
+    {
+      text: "Thanks! ",
+      spriteAlternations: 2,
+      sprites: [19, 20],
+      endSprite: 30,
+      nextDialogIndex: 13
+    },
+    {
+      text: "No problem! If you change your mind, just ask and i'll give it to you. ", //oh right! you already have it
+      spriteAlternations: 3,
+      sprites: [21, 22],
+      endSprite: 22,
+      nextDialogIndex: 12
+    },
+    {
+      text: "... ", //oh right! you already have it
+      spriteAlternations: 0,
+      sprites: [24],
+      endSprite: 24,
+      nextDialogIndex: 13
+    },
+    {
+      text: "Thank you for visiting my website! ",
+      spriteAlternations: 3,
+      sprites: [25, 26],
+      endSprite: 26,
+      nextDialogIndex: 14
+    },
+
+    {
+      text: "If you have any further questions, just let me know!",
+      spriteAlternations: 4,
+      sprites: [27, 28],
+      endSprite: 28,
+      nextDialogIndex: 15
+    },
+    {
+      text: "...", //----------------------------- back to 15
+      spriteAlternations: 0,
+      sprites: [19],
+      endSprite: 30,
+      nextDialogIndex: 16
+    },
+    {
+      text: "Hi again! is there something I can help you with?", //------btns again
+      spriteAlternations: 7,
+      sprites: [5, 6],
+      endSprite: 6,
+      nextDialogIndex: 16
+    },
+    {
+     text: "Sure! Here!",
+      spriteAlternations: 1,
+      sprites: [13,17, 18, 19, 20],
+      endSprite: 30,
+      nextDialogIndex: 15
+    },
+    {
+     text: "You can reach me out by mail or through linkedin, throuh the icaons down below.",
+     spriteAlternations: 6,
+      sprites: [7, 8],
+      endSprite: 30,
+      nextDialogIndex: 15
+    },
+    {
+     text: " Well, besides working on this website, I've been learning React and Three.js. I've also been refreshing my SQL skills.",
+     spriteAlternations: 9,
+      sprites: [9, 10],
+      endSprite: 30,
+      nextDialogIndex: 15
+    },
+    {
+     text: " Sure thing!",
+     spriteAlternations: 3,
+      sprites: [25, 26],
+      endSprite: 26,
+      nextDialogIndex: 2
+    },
+  ];
+
+  isButtonShown = signal(false);
+  isButtonsResumeShown = signal(false);
+  isButtonsEndShown = signal(false);
+  isNameShown = signal(false);
+  isDialogueShown = signal(true);
+
+  private isAnimating = false; //to redraw ends sprite on resize if not
+
   private spritesheetImg = new Image();
+
   private currDialogIndex = 0;
-  private canvasIsAcceptingClicks = false;
+  currDialogText = signal("...");
+  private isCanvasAcceptingClicks = false;
   private frameCount = 0;
+  private currAlternatingTimes = -1;
+  private currDialogSpriteIndex = -1;
+
+  private animationFrameId = 0;
 
   @HostListener('window:resize')
   onResize() {
 
     this.scaleCanvas();
-     this.ctx!.drawImage(this.spritesheetImg, 512, 32, 512, 512, (this.width-512)/2, 0, 512, 512)
+    if (this.currDialogIndex === -1) { //aslo review
+      this.ctx!.drawImage(this.spritesheetImg, 0, 32, 512, 512, (this.width - 512) / 2, 0, 512, 512)
+    }
+    else {
+      if (this.isAnimating === false) { //must redraw curr end sprite
+        const currSpritePosX = this.dialogs[this.currDialogIndex].endSprite;
+        this.ctx!.drawImage(this.spritesheetImg, 512 * currSpritePosX, 32, 512, 512, (this.width - 512) / 2, 0, 512, 512)
+      }
+    }
+
   }
 
-    ngAfterViewInit() {
+  ngAfterViewInit() {
     this.scaleCanvas();
     this.loadAssets();
   }
@@ -51,36 +233,110 @@ export class Who {
 
     if (ctx) {
       this.ctx = ctx;
-      
+
     }
   }
 
   loadAssets() {
 
+
     this.spritesheetImg.src = 'spritesheet512.png';
     this.spritesheetImg.onload = () => { //x spritestart, yspritestart (from top), x spritesize, yspritesize, x canvas start, y canvasstart
-      this.ctx!.drawImage(this.spritesheetImg, 512, 32, 512, 512, (this.width-512)/2, 0, 512, 512) //can play with gamew rapper heigth and here sy
-      this.canvasIsAcceptingClicks = true;
+      this.ctx!.drawImage(this.spritesheetImg, 0, 32, 512, 512, (this.width - 512) / 2, 0, 512, 512) //can play with gamew rapper heigth and here sy
+      this.isCanvasAcceptingClicks = true;
     };
 
   }
 
   handleClick() {
 
-    if (this.canvasIsAcceptingClicks === true) {
+    if (this.isCanvasAcceptingClicks === true) { //mihgt have to cancel out curr animate here if clicks are accepted wihlle animating
+      if (this.currDialogIndex === 9) {
+        this.isCanvasAcceptingClicks = false;
+        this.isNameShown.set(false);
+        this.isDialogueShown.set(false);
+        this.isButtonShown.set(true);
+        this.isButtonsResumeShown.set(true);
+      }else if (this.currDialogIndex === 16) {
+        this.isCanvasAcceptingClicks = false;
+        this.isNameShown.set(false);
+        this.isDialogueShown.set(false);
+        this.isButtonShown.set(true);
+        this.isButtonsEndShown.set(true);
+      } else {
+        this.currDialogIndex = this.dialogs[this.currDialogIndex].nextDialogIndex;
+        if (this.currDialogIndex == 2) {
+          this.isNameShown.set(true);
+        }
+        this.isCanvasAcceptingClicks = false;
         this.showDialog();
-      
+      }
     }
 
 
-    //if in choice, ignore
 
   }
 
-  showDialog(){ 
+  handleChoiceButtonClick(nextDialogIndex: number) {
+
+    this.currDialogIndex = nextDialogIndex;
+    this.showDialog();
+    this.isButtonsEndShown.set(false);
+    this.isButtonsResumeShown.set(false);
+    this.isButtonShown.set(false);
+    this.isNameShown.set(true);
+    this.isDialogueShown.set(true);
+    this.isCanvasAcceptingClicks = true;
+  }
+
+  showDialog() {
+    this.currAlternatingTimes = this.dialogs[this.currDialogIndex].spriteAlternations;
+    this.currDialogSpriteIndex = 0;
+    this.animate();
+    this.isAnimating = true;
+    this.currDialogText.set(this.dialogs[this.currDialogIndex].text);
+
+    this.isCanvasAcceptingClicks = true; // on 8 shouldnt be allowed
+
+
     //dialog count
     // number of times to 
 
+  }
+
+  animate() { //somewhat contrived, review
+
+    this.ctx!.clearRect(0, 0, this.width, this.height);
+
+    var currSpritePosX = this.dialogs[this.currDialogIndex].sprites[this.currDialogSpriteIndex];
+
+    if (this.currAlternatingTimes > 0) {
+
+      if ((this.frameCount) <= 10) {
+        this.ctx!.drawImage(this.spritesheetImg, 512 * currSpritePosX, 32, 512, 512, (this.width - 512) / 2, 0, 512, 512)
+        this.frameCount++;
+        if (this.frameCount === 11) {
+          this.frameCount = 0;
+          this.currDialogSpriteIndex++;
+          if (this.currDialogSpriteIndex === this.dialogs[this.currDialogIndex].sprites.length) { //circle all
+            this.currAlternatingTimes--;
+            this.currDialogSpriteIndex = 0;
+
+          }
+
+        }
+      }
+    } else {
+      currSpritePosX = this.dialogs[this.currDialogIndex].endSprite;
+      this.ctx!.drawImage(this.spritesheetImg, 512 * currSpritePosX, 32, 512, 512, (this.width - 512) / 2, 0, 512, 512)
+      this.isAnimating = false;
+      //tirgger lcik here if ===6 after timeout
+      return;
+
+    }
+
+
+    this.animationFrameId = requestAnimationFrame(() => this.animate());
   }
 
 
